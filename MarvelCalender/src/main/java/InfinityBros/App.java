20a5751cd4ebefcc01452a22fa5d72981e038a54
Robert Basson <robertba@bbd.co.za>
2019-04-23 14:50:36 +0200

    Repo

---
+++ MarvelCalender/src/main/java/InfinityBros/App.java
@@ -0,0 +1,13 @@
+package InfinityBros;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
